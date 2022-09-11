38bb5b49c211db828a60689ae117c7c323025910
dharmesh0103 <dharmeshvarshney8@gmail.com>
2022-09-11 15:41:00 +0530

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dharmesh/sauvc_ws/src
+# Build directory: /home/dharmesh/sauvc_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("freefloating_gazebo")
